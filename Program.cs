edf857bff763e28183dac01fc516608ece287a1f
zodraz <zodraz@gmail.com>
2022-06-05 20:28:19 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello AWS AppRunner !!");
+app.MapGet("/api/coffee", () => new List<dynamic> {
+    new { CoffeeId = "cappucino", CoffeeName = "Cappucino" },
+    new { CoffeeId = "latte", CoffeeName = "Latte" },
+    new { CoffeeId = "mocha", CoffeeName = "Mocha" },
+    new { CoffeeId = "americano", CoffeeName = "Americano" },
+    new { CoffeeId = "macchiato", CoffeeName = "Macchiato" },
+    new { CoffeeId = "frappe", CoffeeName = "Frappe" },
+    new { CoffeeId = "corretto", CoffeeName = "Corretto" },
+    new { CoffeeId = "affogato", CoffeeName = "Affogato" },
+    new { CoffeeId = "filtercoffee", CoffeeName = "Filter Coffee" },
+});
 
 app.Run();
